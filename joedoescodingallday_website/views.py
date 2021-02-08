b84f264399e4ad189da71babe8eb13b806919152
Joseph Chu <jchucodes@gmail.com>
2021-02-08 10:24:22 -0500

    refactor: :art: improved the urlpatterns
    I reverted the page back to its original state in lieu of the aforementioned state that was using render template
    
    none

--- joedoescodingallday_website/views.py
+++ joedoescodingallday_website/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hi")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
