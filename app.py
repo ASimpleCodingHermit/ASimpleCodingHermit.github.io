8ea0a95c36b6664f8ef9222ba9b324a5f9b60879
Joseph Chu <jchucodes@gmail.com>
2021-02-09 07:05:46 -0500

    refactor: :art: Updated home.html with dynamic Jinja code for calls from other files
    None

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Joseph Chu',
+        'title': 'Blog Post Title',
+        'content': 'Blog Post Content',
+        'date_posted': 'Feburary 8, 2021'
+    },
+    {
+        'author': 'Joshanne Chu',
+        'title': 'Blog Post Title 2',
+        'content': 'Blog Post Content 2',
+        'date_posted': 'Feburary 14, 2021'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
